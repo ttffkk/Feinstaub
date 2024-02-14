61bef4ca180d1ab765179a6b4d8ec5db00fe867e
ttffkk <tfkaminski@gmail.com>
2024-02-14 14:04:51 +0100

    Corrected the SQL

--- Feinstaub.sql
+++ Feinstaub.sql
@@ -1,17 +1,14 @@
 create table DHT22(
 DHT22_id int primary key auto_increment,
 luftfeuchtigkeit float,
-zeitstempel varchar(255), 
+zeitstempel Datetime,
 temperatur float
 );
-
-alter table DHT22 auto_increment = 1;
 
 create table SDS011(
 SDS011_id int primary key auto_increment,
 PM10 float,
 PM25 float, 
-zeitstempel varchar(255)
+zeitstempel datetime
 );
 
-alter table SDS011 auto_increment = 1;
