e0ef0b3de690644ea2bdfc034767054c4710597d
ttffkk <tfkaminski@gmail.com>
2024-02-21 14:10:40 +0100

    firstattempt of download

--- CSV_Download/CSV_Downloader.py
+++ CSV_Download/CSV_Downloader.py
@@ -1,5 +1,8 @@
-import urllib.request
+import urllib
+
+import requests
 import datetime
+import os
 
 
 def download(url):
@@ -9,11 +12,14 @@ def download(url):
     data = urllib.request.urlopen(url).read()
     return data
 
-
 def save(data, filename):
-    """Save data into the given filename."""
-    with open(filename, 'wt') as f:
-        f.write(data)
+    'Save data into the given filename.'
+    if filename.endswith(".gz"):
+        with open(filename, 'wb') as f:
+            f.write(data)
+    else:
+        with open(filename, 'wt') as f:
+            f.write(data)
 
 
 def get_dates_2022():
@@ -23,3 +29,13 @@ def get_dates_2022():
         dates_2022.append(current_date.strftime("%Y-%m-%d"))
         current_date += datetime.timedelta(days=1)
     return dates_2022
+
+
+filepath = "csv"
+dates_2022 = get_dates_2022()
+for date in dates_2022:
+    url = f"https://archive.sensor.community/2022/{date}/{date}_sds011_sensor_3659.csv.gz"
+    filepath = f"gz/{date}.csv.gz"
+    print(f"Downloading from",url)
+    data = download(url)
+    save(data, filepath)
