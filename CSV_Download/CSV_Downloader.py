bba54809b53271dd1f095ffa0dc4f5777d352a80
ttffkk <tfkaminski@gmail.com>
2024-02-21 13:24:47 +0100

    Added date method

--- CSV_Download/CSV_Downloader.py
+++ CSV_Download/CSV_Downloader.py
@@ -2,7 +2,6 @@ import urllib.request
 import datetime
 
 
-
 def download(url):
     """
     Download and return data from the given URL.
@@ -10,8 +9,17 @@ def download(url):
     data = urllib.request.urlopen(url).read()
     return data
 
+
 def save(data, filename):
-    'Save data into the given filename.'
+    """Save data into the given filename."""
     with open(filename, 'wt') as f:
         f.write(data)
 
+
+def get_dates_2022():
+    dates_2022 = []
+    current_date = datetime.date(2022, 1, 1)
+    while current_date.year == 2022:
+        dates_2022.append(current_date.strftime("%Y-%m-%d"))
+        current_date += datetime.timedelta(days=1)
+    return dates_2022
