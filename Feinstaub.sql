bb7fd61def8d88ce4d5e3e7dfe705b9f07d0e17b
ttffkk <73671836+ttffkk@users.noreply.github.com>
2024-03-13 13:20:10 +0100

    Update Feinstaub.sql

--- Feinstaub.sql
+++ Feinstaub.sql
@@ -1,1 +1,13 @@
-select * from dht22
+create table DHT22(
+DHT22_id int primary key auto_increment,
+luftfeuchtigkeit float,
+zeitstempel Datetime,
+temperatur float
+);
+
+create table SDS011(
+SDS011_id int primary key auto_increment,
+PM10 float,
+PM25 float, 
+zeitstempel datetime
+);
