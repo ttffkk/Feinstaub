8d54b8b15af41efe0a25726e4ff981a39bb74356
ttffkk <tfkaminski@gmail.com>
2024-03-13 13:10:00 +0100

    added filecatcher, misses filecreator

--- Feinstaub/Feinstaub.py
+++ Feinstaub/Feinstaub.py
@@ -13,11 +13,12 @@ config = {
 # Establish a connection to the database
 cnx = mysql.connector.connect(**config)
 
+
 # Function to insert data into the table
 def insertDataIntoTable(data):
     cursor = cnx.cursor()
     cursor.execute("INSERT INTO SDS011 (PM10, PM25, zeitstempel) VALUES (%s, %s, %s)", data)
-    cnx.commit()
+    cnx.commit()  # Commit the transaction
     cursor.close()
 
 
@@ -35,6 +36,7 @@ def fileToTable(file):
         csv_reader = csv.reader(csvfile, delimiter=';')
         next(csv_reader)  # Skip the header row if it exists
         for row in csv_reader:
+            print(row)
 
             # Assuming the CSV columns are in the order PM10, PM25, and zeitstempel
             sensor_id, sensor_type, location, lat, lon, timestamp, P1, durP1, ratioP1, P2, durP2, ratioP2 = row
@@ -42,11 +44,4 @@ def fileToTable(file):
             insertDataIntoTable(data)
 
     # Close the database connection
-
-        # Assuming the CSV columns are in the order PM10, PM25, and zeitstempel
-        sensor_id, sensor_type, location, lat, lon, timestamp, P1, durP1, ratioP1, P2, durP2, ratioP2 = row
-        data = (P1, P2, timestamp)
-        insertDataIntoTable(data)
-
-# Close the database connection
-cnx.close()
+    cnx.close()
