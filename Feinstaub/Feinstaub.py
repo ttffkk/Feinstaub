31a53d033957352f08ab3140b5878232248db783
Daniel Bohr <141116940+DanielBohrCosinexDe@users.noreply.github.com>
2024-03-13 13:39:34 +0100

    Codeanpassung + Sql import in der Feinstaub.py

--- Feinstaub/Feinstaub.py
+++ Feinstaub/Feinstaub.py
@@ -1,28 +1,48 @@
+import csv
+import os
 import mysql.connector
 
 config = {
     'user': 'root',
-    'password': 'Arian2001',
-    'host': '127.0.0.1:3306',
-    'database': 'Feinstaub',
-    'raise_on_warnings': True #Alles erledigt. Habibi
+    'password': 'DkEab99729777',
+    'host': '127.0.0.1',  # Remove the port number from the host
+    'database': 'feinstaub',
+    'raise_on_warnings': True
 }
+
+# Establish a connection to the database
 cnx = mysql.connector.connect(**config)
 
-
-# Funktion, um Daten in die Tabelle einzufügen
-def insertDataIntoTable():
+# Function to insert data into the table
+def insertDataIntoTable(data):
     cursor = cnx.cursor()
-    cursor.execute("insert into feinstaub.sds011 (PM10, PM25, zeitstempel) values (?, ?, ?)")
-    cursor.execute("insert into feinstaub.dht22 (luftfeuchtigkeit, zeitstempel, temperatur) values (?, ?, ?)")
-    # csv Daten mit müssen noch gesettet werden
-    cnx.close()
+    cursor.execute("INSERT INTO SDS011 (PM10, PM25, zeitstempel) VALUES (%s, %s, %s)", data)
+    cnx.commit()
+    cursor.close()
 
-# Funktion, um Daten aus der Tabelle heraus zu bekommen
-def getDataFromTable():
-    cursor = cnx.cursor()
-    cursor.execute("select * from feinstaub.sds011 where zeitstempel like 2022-03-14")
-    ergebnisse = cursor.fetchall()
-    for row in ergebnisse:
-        print(row)
-    cnx.close()
+
+def fileCatcher():
+    csv_folder = '../CSV_Download/csv'
+    for filename in os.listdir(csv_folder):
+        if filename.endswith('.csv'):
+            full_path = os.path.join(csv_folder, filename)
+            fileToTable(full_path)
+
+
+# Open the CSV file and read the data
+def fileToTable(file):
+    with open(file, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=';')
+        next(csv_reader)  # Skip the header row if it exists
+        for row in csv_reader:
+
+            # Assuming the CSV columns are in the order PM10, PM25, and zeitstempel
+            sensor_id, sensor_type, location, lat, lon, timestamp, P1, durP1, ratioP1, P2, durP2, ratioP2 = row
+            data = (P1, P2, timestamp)
+            insertDataIntoTable(data)
+
+    # Close the database connection
+
+
+fileCatcher()
+cnx.close()
