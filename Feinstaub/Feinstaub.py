a3578522e7cda4961e08bdac2ee3c72a9e7c4240
ttffkk <tfkaminski@gmail.com>
2024-03-20 11:55:57 +0100

    adjusted naming

--- Feinstaub/Feinstaub.py
+++ Feinstaub/Feinstaub.py
@@ -15,23 +15,23 @@ cnx = mysql.connector.connect(**config)
 
 
 # Function to insert data into the table
-def insertDataIntoTable(data):
+def insert_data_into_table(data):
     cursor = cnx.cursor()
     cursor.execute("INSERT INTO SDS011 (PM10, PM25, zeitstempel) VALUES (%s, %s, %s)", data)
     cnx.commit()
     cursor.close()
 
 
-def fileCatcher():
+def file_catcher():
     csv_folder = '../CSV_Download/csv'
     for filename in os.listdir(csv_folder):
         if filename.endswith('.csv'):
             full_path = os.path.join(csv_folder, filename)
-            fileToTable(full_path)
+            file_to_table(full_path)
 
 
 # Open the CSV file and read the data
-def fileToTable(file):
+def file_to_table(file):
     with open(file, 'r') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=';')
         next(csv_reader)  # Skip the header row if it exists
@@ -40,18 +40,18 @@ def fileToTable(file):
             # Assuming the CSV columns are in the order PM10, PM25, and zeitstempel
             sensor_id, sensor_type, location, lat, lon, timestamp, P1, durP1, ratioP1, P2, durP2, ratioP2 = row
             data = (P1, P2, timestamp)
-            insertDataIntoTable(data)
+            insert_data_into_table(data)
 
     # Close the database connection
 
         # Assuming the CSV columns are in the order PM10, PM25, and zeitstempel
         sensor_id, sensor_type, location, lat, lon, timestamp, P1, durP1, ratioP1, P2, durP2, ratioP2 = row
         data = (P1, P2, timestamp)
-        insertDataIntoTable(data)
+        insert_data_into_table(data)
 
     # Close the database connection
 
 
-fileCatcher()
+file_catcher()
 cnx.close()
 # Close the database connection
