7abf872a74f368a3f4f41141865bfe83affd78fd
ttffkk <tfkaminski@gmail.com>
2024-04-10 14:00:39 +0200

    request

---
+++ request.sql
@@ -0,0 +1,6 @@
+SELECT
+  MIN(temperatur) AS min_temperatur,
+  MAX(temperatur) AS max_temperatur,
+  AVG(temperatur) AS durchschnittliche_temperatur
+FROM feinstaub.DHT22
+WHERE zeitstempel = '2022-03-14';
